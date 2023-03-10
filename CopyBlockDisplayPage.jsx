b1a191719d553ed93c5d569f1046230cbe856d96
neyona <40152041+neyona@users.noreply.github.com>
2023-03-10 11:17:58 -0500

    Added CopyBlockPage

---
+++ CopyBlockDisplayPage.jsx
@@ -0,0 +1,20 @@
+import Card from 'react-bootstrap/Card';
+import Blocks from './Blocks';
+
+const CopyBlockDisplayPage = () => {
+
+  return (
+    <>
+      <Card>
+        <Card.Title>
+          <h1>CopyBlocks and their Themes</h1>
+        </Card.Title>
+        <Card.Body>
+          <Blocks />
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default CopyBlockDisplayPage;
